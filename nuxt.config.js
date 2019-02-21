866ab73a501ab318dd8c2cc2fcd597e8e207545e
Kris <kristhesavage@gmail.com>
2019-02-21 15:35:13 +0100

    Load enviromental variables in the nuxt.config.js as well.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,8 @@
+require('dotenv').config({
+  path: 'RadiusGUI.env',
+});
+
 const pkg = require('./package');
-const fs = require('fs');
 
 module.exports = {
   mode: 'universal',
@@ -9,36 +12,39 @@ module.exports = {
    */
   head: {
     title: pkg.name,
-    meta: [{
-        charset: 'utf-8'
+    meta: [
+      {
+        charset: 'utf-8',
       },
       {
         name: 'viewport',
-        content: 'width=device-width, initial-scale=1'
+        content: 'width=device-width, initial-scale=1',
       },
       {
         hid: 'description',
         name: 'description',
-        content: pkg.description
-      }
+        content: pkg.description,
+      },
     ],
-    link: [{
+    link: [
+      {
         rel: 'icon',
         type: 'image/x-icon',
-        href: '/favicon.ico'
+        href: '/favicon.ico',
       },
       {
         rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
-      }
-    ]
+        href:
+          'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons',
+      },
+    ],
   },
 
   /*
    ** Customize the progress-bar color
    */
   loading: {
-    color: '#fff'
+    color: '#fff',
   },
 
   /*
@@ -46,31 +52,26 @@ module.exports = {
    */
   css: [
     '@/assets/style/app.styl',
-    '~/node_modules/vue-snotify/styles/material.css'
+    '~/node_modules/vue-snotify/styles/material.css',
   ],
 
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [
-    '@/plugins/vuetify',
-	'@/plugins/snotify'
-  ],
+  plugins: ['@/plugins/vuetify', '@/plugins/snotify'],
 
   /*
    ** Nuxt.js modules
    */
-  modules: [
-    '@nuxtjs/axios',
-  ],
+  modules: ['@nuxtjs/axios'],
 
   axios: {
     credentials: false,
-    baseURL: 'https://localhost:8443/api/'
+    baseURL: process.env.BASE_URL,
   },
 
   env: {
-    ELK_HOST: process.env.ELK_HOST
+    ELK_HOST: process.env.ELK_HOST,
   },
 
   /*
@@ -88,9 +89,9 @@ module.exports = {
           enforce: 'pre',
           test: /\.(js|vue)$/,
           loader: 'eslint-loader',
-          exclude: /(node_modules)/
+          exclude: /(node_modules)/,
         });
       }
-    }
-  }
+    },
+  },
 };
